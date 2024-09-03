9a50819de0fec6960942c3464711082ffe830927
igorvianaf <igor.vianaf1@hotmail.com>
2024-09-03 18:15:16 -0300

    atualizando resposta para uso de funcao

--- ex05.py
+++ ex05.py
@@ -1,9 +1,6 @@
-string = input('Digite o palavra/frase para eu te mostrar ele invertido: ')
-#variavel para armazenar cada letra ao contrario
-string_contrario = ''
+def inverter_string(string):
+    return string[::-1]
 
-#loop para pegar letra do final para o inicio e armazenar em variavel
-for letra in string[::-1]:
-    string_contrario += letra
 
-print(string_contrario)
+string_entrada = inverter_string(input('Digite o palavra/frase para eu te mostrar ele invertido: '))
+print(string_entrada)
