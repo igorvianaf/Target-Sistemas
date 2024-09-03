3c95f275f90e7d4fe1464af2b7a4c05ec87269a4
igorvianaf <igor.vianaf1@hotmail.com>
2024-09-03 18:16:27 -0300

    add method

--- ex05.py
+++ ex05.py
@@ -3,4 +3,4 @@ def inverter_string(string):
 
 
 string_entrada = inverter_string(input('Digite o palavra/frase para eu te mostrar ele invertido: '))
-print(string_entrada)
+print(string_entrada.strip())
